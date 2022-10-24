95a08fe41ba30cab0a46a8fc596b465ff5a35641
Mike Wadden <mike.wadden@keyin.com>
2022-10-24 10:44:56 -0230

    Added first class

---
+++ src/main/java/com/general/Member.java
@@ -0,0 +1,12 @@
+package com.general;
+
+import java.util.Date;
+
+public class Member extends Person{
+
+    private String memberID;
+    private Date startDate;
+    private Membership membership;
+
+
+}

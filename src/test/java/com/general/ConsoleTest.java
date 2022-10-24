ddff1d3010ae77fc093a4043403016e26ecb5e5b
Tarsolan <aridgeley@msn.com>
2022-10-24 09:40:27 -0230

    Test Test

--- src/test/java/com/general/ConsoleTest.java
+++ src/test/java/com/general/ConsoleTest.java
@@ -1,4 +1,11 @@
 package com.general;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ConsoleTest {
+    @Test
+    public void testPrint(){
+        Assertions.assertEquals("Time", "Time");
+    }
 }

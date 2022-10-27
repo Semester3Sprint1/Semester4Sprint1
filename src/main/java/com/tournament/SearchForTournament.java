742393bfd0f126242d7fc2b1d3c0b944845e38ce
Mike Wadden <mike.wadden@keyin.com>
2022-10-27 09:12:54 -0230

    Small fixes for Testing

--- src/main/java/com/tournament/SearchForTournament.java
+++ src/main/java/com/tournament/SearchForTournament.java
@@ -9,10 +9,8 @@ public class SearchForTournament {
 
     public static ArrayList<Tournament> tList = new ArrayList<>();
 
-    public static int tournamentCount = tList.size();
-
     //to find a tournament in the list by its name // Note prob should change to id
-    public static Tournament findTournament(String name){
+    public static Tournament findTournament(String name, ArrayList<Tournament> tList){
         Tournament tournament = tList.stream()
                 .filter(m-> m.getName().toUpperCase().equals(name.toUpperCase())).findAny().orElse(null);
         return tournament;
